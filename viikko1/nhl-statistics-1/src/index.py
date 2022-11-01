0fe62769c78af6b453603706ca6d478fe5328e71
HYTapio <tapio.uzal@helsinki.fi>
2022-11-01 22:38:20 +0200

    added sortby

--- viikko1/nhl-statistics-1/src/index.py
+++ viikko1/nhl-statistics-1/src/index.py
@@ -1,4 +1,5 @@
-from statistics import Statistics
+from statistics import Statistics, SortBy
+
 from player_reader import PlayerReader
 
 
@@ -6,16 +7,33 @@ from player_reader import PlayerReader
 def main():
     stats = Statistics(PlayerReader())
     philadelphia_flyers_players = stats.team("PHI")
-    top_scorers = stats.top(10)
 
     print("Philadelphia Flyers:")
     for player in philadelphia_flyers_players:
         print(player)
+    print()
 
+    # järjestetään kaikkien tehopisteiden eli maalit+syötöt perusteella
     print("Top point getters:")
-    for player in top_scorers:
+    for player in stats.top(10, SortBy.POINTS):
         print(player)
+    print()
 
+    # metodi toimii samalla tavalla kuin yo. kutsu myös ilman toista parametria
+    print("Top point getters:")
+    for player in stats.top(10):
+        print(player)
+    print()
 
+    # järjestetään maalien perusteella
+    print("Top point goal scorers:")
+    for player in stats.top(10, SortBy.GOALS):
+        print(player)
+    print()
+
+    # järjestetään syöttöjen perusteella
+    print("Top by assists:")
+    for player in stats.top(10, SortBy.ASSISTS):
+        print(player)
 if __name__ == "__main__":
     main()
