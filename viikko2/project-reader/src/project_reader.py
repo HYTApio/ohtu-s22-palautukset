39a715dbd5ce0242a0e581c4f63e1fb480fcbfb2
HYTapio <tapio.uzal@helsinki.fi>
2022-11-10 16:58:50 +0200

    added viikko2

---
+++ viikko2/project-reader/src/project_reader.py
@@ -0,0 +1,21 @@
+from urllib import request
+from project import Project
+import toml
+
+
+class ProjectReader:
+    def __init__(self, url):
+        self._url = url
+
+    def get_project(self):
+        # tiedoston merkkijonomuotoinen sisältö
+        content = request.urlopen(self._url).read().decode("utf-8")
+        data = (toml.loads(content))
+
+        name = data["tool"]["poetry"]["name"]
+        description = data["tool"]["poetry"]["description"]
+        dependencies = data["tool"]["poetry"]["dependencies"]
+        devdependencies = data["tool"]["poetry"]["dev-dependencies"]
+
+        # deserialisoi TOML-formaatissa oleva merkkijono ja muodosta Project-olio sen tietojen perusteella
+        return Project(name, description, dependencies, devdependencies)
